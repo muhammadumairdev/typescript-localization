2f45b75a904bfedba223455033c14537117313ee
muhammadumairdev <umair68094@gmail.com>
2023-11-23 16:19:43 +0500

    typescript-localization

---
+++ index.ts
@@ -0,0 +1,9 @@
+import i18next from "./i18n";
+
+i18next.changeLanguage("en");
+
+const greetingMessage = i18next.t("common:greetings");
+const welcomeMessage = i18next.t("common:welcome", { name: "John" });
+
+console.log("greeting:", greetingMessage);
+console.log("welcome:", welcomeMessage);
